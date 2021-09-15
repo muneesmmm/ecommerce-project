7a51d9e223afa2be70ae2d89d28d843c2bc7e6c7
Muhammed Munees <muneesmmm@gmail.com>
2021-09-15 15:10:46 +0530

    update components

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
-const shopeRoutes = require('./routes/shopeRoutes')
+const storeRoutes = require('./routes/storeRoutes')
 
 connectDB();
 const app = express();
 app.use(express.json());
 app.use('/api/user',userRoutes)
-app.use('/api/shope',shopeRoutes)
+app.use('/api/store',storeRoutes)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`server running on port ${PORT}`));
