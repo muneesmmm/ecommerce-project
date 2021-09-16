5a254c77548242d9b4c1e15498a44c2c1f1db989
Muhammed Munees <muneesmmm@gmail.com>
2021-09-16 10:06:02 +0530

    add sample product cards

--- frontend/src/Components/User/Products/Products.js
+++ frontend/src/Components/User/Products/Products.js
@@ -1,9 +1,78 @@
 import React from 'react'
+import './Style.css'
 function Products() {
     return (
-        <div>
-   
-        </div>
+    
+            <div className="homescreen">
+                <h2 className="homescreen__title">Latest Products</h2>
+                <div className="homescreen__products">
+                    <div className="product">
+                        <img src={"https://www.jaipurite.com/images/products/img-girls-tunic-tops.jpg"} alt="" />
+
+                        <div className="product__info">
+                            <p className="info__name">Top</p>
+
+                            <p className="info__description">aabbccddeeffggee...</p>
+
+                            <p className="info__price">$1234</p>
+
+                            <a nhref="#" className="info__button">
+                                View
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <img src={"https://www.jaipurite.com/images/products/img-girls-tunic-tops.jpg"} alt="" />
+
+                        <div className="product__info">
+                            <p className="info__name">Top</p>
+
+                            <p className="info__description">aabbccddeeffggee...</p>
+
+                            <p className="info__price">$1234</p>
+
+                            <a nhref="#" className="info__button">
+                                View
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <img src={"https://www.jaipurite.com/images/products/img-girls-tunic-tops.jpg"} alt="" />
+
+                        <div className="product__info">
+                            <p className="info__name">Top</p>
+
+                            <p className="info__description">aabbccddeeffggee...</p>
+
+                            <p className="info__price">$1234</p>
+
+                            <a nhref="#" className="info__button">
+                                View
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="product">
+                        <img src={"https://www.jaipurite.com/images/products/img-girls-tunic-tops.jpg"} alt="" />
+
+                        <div className="product__info">
+                            <p className="info__name">Top</p>
+
+                            <p className="info__description">aabbccddeeffggee...</p>
+
+                            <p className="info__price">$1234</p>
+
+                            <a nhref="#" className="info__button">
+                                View
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        
     )
 }
 
