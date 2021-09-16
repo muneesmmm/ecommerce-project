3ad4f1ef087e4a08959a69b738dd705566827c38
Abdul Hakeem <haq9966@gmail.com>
2021-09-16 21:47:30 +0530

    hard coded carousel in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
+import Carousel from './Components/User/Carousel/Carousel';
 import './App.css';
 import UserLogin from './Components/User/Login/UserLogin';
 import Products from './Components/User/Products/Products';
 import Signup from './Components/User/Signup/Signup';
-
+import './Components/User/Carousel/carousel.css';
 function App() {
   return (
     <div>
         {/* <UserLogin/> */}
         {/* <Signup/> */}
+        <Carousel />
         <Products/>
         
     </div>
