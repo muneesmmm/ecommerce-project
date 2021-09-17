0f233b6e8cc1a7af0604476b4a5bc431dac82f23
Abdul Hakeem <haq9966@gmail.com>
2021-09-17 17:54:29 +0530

    started working on navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import UserLogin from './Components/User/Login/UserLogin';
 import Products from './Components/User/Products/Products';
 import Signup from './Components/User/Signup/Signup';
 import './Components/User/Carousel/carousel.css';
+import Navbar from './Components/User/Navbar/Navbar';
 function App() {
   return (
     <div>
         {/* <UserLogin/> */}
         {/* <Signup/> */}
+        <Navbar/>
         <Carousel />
         <Products/>
         
